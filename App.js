0bbd882a4368310c175824d2e3988d99a6880aff
Anushka Rakesh <93435221+AnushkaRakesh@users.noreply.github.com>
2023-06-04 11:44:39 +0530

    App.js

--- App.js
+++ App.js
@@ -93,7 +93,7 @@ const styles = StyleSheet.create({
   searchInput: {
     height: 40,
     borderWidth: 1,
-    borderColor: 'lightblack',
+    borderColor: 'black',
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
